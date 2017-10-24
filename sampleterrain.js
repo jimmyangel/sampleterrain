d240904d1f5ca600d3c7627f1d6877d00346a400
baloola <baloola-mu@hotmail.com>
2017-10-24 09:17:48 +0200

    -level- option was removed from the cli

--- sampleterrain.js
+++ sampleterrain.js
@@ -7,14 +7,14 @@ var fs = require('fs');
 console.warn('Sampling terrain...');
 
 var options = cli.parse({
-    file: ['f', 'GeoJson File', 'file'],
-    level: ['l', 'Terrain level of detail', 'int', 11]
+    file: ['f', 'GeoJson File', 'file']
+  
 });
 
 if (options.file) {
   fs.readFile(options.file, 'utf8', function(err, data) {
     if (err) {throw err;}
-    sampleterrain.sample(JSON.parse(data), options.level, function(e, o) {
+    sampleterrain.sample(JSON.parse(data), function(e, o) {
       if (e) {
         console.warn('Sample terrain error', e);
       } else {
