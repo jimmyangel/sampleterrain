8950fd671afe97296b6330b3f5e47d9b5701378d
Ricardo Morin <morinricardo@gmail.com>
2018-04-18 14:30:05 -0700

    Add GeoJSON validation & additional tests

--- spec/sampleterrain-spec.js
+++ spec/sampleterrain-spec.js
@@ -18,6 +18,14 @@ describe('my test suite',function(){
     });
   });
 
+  it('should add elevation coordinates to a large GeoJSON json file with long/lat', function(done) {
+    var geojson = require('../sampledata/test3.json');
+    sampleterrain.sample(geojson, {level: 6}).then(function(result) {
+      expect(result.features[0].geometry.coordinates[0][0].length).toEqual(3);
+      done();
+    });
+  });
+
   it('should return error if an invalid detail level is used', function(done) {
     var geojson = require('../sampledata/test1.json');
     sampleterrain.sample(geojson, {level: 100}).then(function(result) {
@@ -40,5 +48,26 @@ describe('my test suite',function(){
     });
   });
 
+  it('should return error if an invalid GeoJSON file is used', function(done) {
+    var geojson = require('../sampledata/test0.json');
+    sampleterrain.sample(geojson).then(function(result) {
+      expect(result).toBeFalsy();
+      done();
+    }).catch(function(e) {
+      expect(e).toBeTruthy();
+      done();
+    });
+  });
+
+  it('should return error if GeoJSON file with invalid data is used', function(done) {
+    var geojson = require('../sampledata/test5.json');
+    sampleterrain.sample(geojson).then(function(result) {
+      expect(result).toBeFalsy();
+      done();
+    }).catch(function(e) {
+      expect(e).toBeTruthy();
+      done();
+    });
+  });
 
 });
