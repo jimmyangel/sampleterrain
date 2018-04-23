cd1a63fd71fce09f662782b76c30b59bc8d5960a
Ricardo Morin <morinricardo@gmail.com>
2018-04-22 19:30:09 -0700

    Refactor step 3

--- sampleterrain.js
+++ sampleterrain.js
@@ -1,23 +1,24 @@
 'use strict';
 
-var cli = require('cli');
-var sampleterrain = require('./lib/');
-var fs = require('fs');
+const cli = require('cli');
+const sampleterrain = require('./lib/');
+const fs = require('fs');
 
 console.warn('Sampling terrain...');
 
-var options = cli.parse({
+let options = cli.parse({
     file: ['f', 'GeoJson File', 'file'],
     level: ['l', 'Terrain level of detail', 'int'],
     url: ['u', 'Terrain provider url', 'url']
 });
 
+let fOptions;
 if (options.level || options.url) {
-  var fOptions = {level: options.level, terrainProviderUrl: options.url};
+  fOptions = {level: options.level, terrainProviderUrl: options.url};
 }
 
 if (options.file) {
-  fs.readFile(options.file, 'utf8', function(err, data) {
+  fs.readFile(options.file, 'utf8', (err, data) => {
     if (err) {throw err;}
     sampleterrain.sample(JSON.parse(data), fOptions).then((o) => {
       console.log(JSON.stringify(o));
