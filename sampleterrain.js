3c835872e60c57a8fb50c8b6362571db771e9023
Ricardo Morin <morinricardo@gmail.com>
2018-04-22 22:16:25 -0700

    Tweaks

--- sampleterrain.js
+++ sampleterrain.js
@@ -23,7 +23,7 @@ if (options.file) {
   fs.readFile(options.file, 'utf8', (err, data) => {
     if (err) {throw err;}
     sampleterrain.sample(JSON.parse(data), fOptions).then((o) => {
-      console.warn('Terrain sampled...', new Date() - start);
+      console.warn('Terrain sampled...', new Date() - start, 'ms');
       console.log(JSON.stringify(o));
     }).catch(e => {
       console.warn('Sample terrain error', e);
